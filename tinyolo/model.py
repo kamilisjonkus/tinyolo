e1f290e74360dfb40788a4f7d751f419203f0e08
kamilisjonkus <kamilis.jonk@gmail.com>
2025-03-04 08:11:20 +0200

    Naming refactor

--- tinyolo/model.py
+++ tinyolo/model.py
@@ -4,17 +4,17 @@ from typing import Tuple, List, Callable
 
 
 class ConvBNReLU:
-  def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, groups=1, bias=False):
-    self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias, groups=groups)
-    self.bn = nn.BatchNorm2d(out_channels)
+  def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=0, groups=1, bias=False):
+    self.conv = nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding, bias=bias, groups=groups)
+    self.bn = nn.BatchNorm2d(out_ch)
 
   def __call__(self, x: Tensor) -> Tensor:
     return self.bn(self.conv(x)).relu()
   
 class ConvBNSiLU:
-  def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, groups=1, bias=False):
-    self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias, groups=groups)
-    self.bn = nn.BatchNorm2d(out_channels)
+  def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=0, groups=1, bias=False):
+    self.conv = nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding, bias=bias, groups=groups)
+    self.bn = nn.BatchNorm2d(out_ch)
 
   def __call__(self, x: Tensor) -> Tensor:
     return self.bn(self.conv(x)).silu()
@@ -24,32 +24,32 @@ class RepVGGBlock:
   This code is based on https://github.com/DingXiaoH/RepVGG/blob/main/repvgg.py
   Quantization-Aware version: https://arxiv.org/abs/2212.01593
   '''
-  def __init__(self, in_channels, out_channels, stride=1, groups=1):
-    self.rbr_identity_bn = nn.BatchNorm2d(in_channels) if out_channels == in_channels and stride == 1 else None
-    self.rbr_3x3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, groups=groups, bias=False)
-    self.rbr_3x3_bn = nn.BatchNorm2d(out_channels)
-    self.rbr_1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, groups=groups, bias=False)
-    self.rbr_1x1_bn = nn.BatchNorm2d(out_channels)
+  def __init__(self, in_ch, out_ch, stride=1, groups=1):
+    self.rbr_identity_bn = nn.BatchNorm2d(in_ch) if out_ch == in_ch and stride == 1 else None
+    self.rbr_3x3 = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=stride, padding=1, groups=groups, bias=False)
+    self.rbr_3x3_bn = nn.BatchNorm2d(out_ch)
+    self.rbr_1x1 = nn.Conv2d(in_ch, out_ch, kernel_size=1, stride=stride, padding=0, groups=groups, bias=False)
+    self.rbr_1x1_bn = nn.BatchNorm2d(out_ch)
 
   def __call__(self, x: Tensor) -> Tensor:
     if self.rbr_identity_bn is None: identity_out = 0
     else: identity_out = self.rbr_identity_bn(x)
     return Tensor.relu(self.rbr_3x3_bn(self.rbr_3x3(x)) + self.rbr_1x1_bn(self.rbr_1x1(x)) + identity_out)
 
-def repeat_block(in_chnls, out_chnls, n) -> List[Callable[[Tensor], Tensor]]:
-    return [RepVGGBlock(in_chnls, out_chnls)] + [RepVGGBlock(out_chnls, out_chnls) for _ in range(n - 1)] if n > 1 else []
+def repeat_block(in_ch, out_ch, n) -> List[Callable[[Tensor], Tensor]]:
+    return [RepVGGBlock(in_ch, out_ch)] + [RepVGGBlock(out_ch, out_ch) for _ in range(n - 1)] if n > 1 else []
   
 class CSPSPPFModule:
   # CSP https://github.com/WongKinYiu/CrossStagePartialNetworks
-  def __init__(self, in_channels, out_channels):
-    c_ = int(out_channels * 0.5)  # hidden channels
-    self.cv1 = ConvBNReLU(in_channels, c_, 1, 1)
-    self.cv2 = ConvBNReLU(in_channels, c_, 1, 1)
+  def __init__(self, in_ch, out_ch):
+    c_ = int(out_ch * 0.5)  # hidden ch
+    self.cv1 = ConvBNReLU(in_ch, c_, 1, 1)
+    self.cv2 = ConvBNReLU(in_ch, c_, 1, 1)
     self.cv3 = ConvBNReLU(c_, c_, 3, 1)
     self.cv4 = ConvBNReLU(c_, c_, 1, 1)
     self.cv5 = ConvBNReLU(4 * c_, c_, 1, 1)
     self.cv6 = ConvBNReLU(c_, c_, 3, 1)
-    self.cv7 = ConvBNReLU(2 * c_, out_channels, 1, 1)
+    self.cv7 = ConvBNReLU(2 * c_, out_ch, 1, 1)
 
   def __call__(self, x: Tensor) -> Tensor:
     x1 = self.cv4(self.cv3(self.cv1(x)))
@@ -60,12 +60,12 @@ class CSPSPPFModule:
     return self.cv7(Tensor.cat((y0, y3), dim=1))
 
 class BiFusion:
-  def __init__(self, in_channels, out_channels):
-    self.cv1 = ConvBNReLU(in_channels[0], out_channels, 1, 1)
-    self.cv2 = ConvBNReLU(in_channels[1], out_channels, 1, 1)
-    self.cv3 = ConvBNReLU(out_channels * 3, out_channels, 1, 1)
-    self.upsample = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=2, stride=2)
-    self.downsample = ConvBNReLU(out_channels, out_channels, 3, 2)
+  def __init__(self, in_ch, out_ch):
+    self.cv1 = ConvBNReLU(in_ch[0], out_ch, 1, 1)
+    self.cv2 = ConvBNReLU(in_ch[1], out_ch, 1, 1)
+    self.cv3 = ConvBNReLU(out_ch * 3, out_ch, 1, 1)
+    self.upsample = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=2, stride=2)
+    self.downsample = ConvBNReLU(out_ch, out_ch, 3, 2)
 
   def __call__(self, x: Tuple[Tensor]) -> Tensor:
     x0 = self.upsample(x[0])
@@ -74,13 +74,13 @@ class BiFusion:
     return self.cv3(Tensor.cat((x0, x1, x2), dim=1))
 
 class EfficientRep:
-  def __init__(self, channels_list, num_repeats):
-    self.stem = [RepVGGBlock(3, channels_list[0], stride=2)]
-    self.ERBlock_2 = [RepVGGBlock(channels_list[0], channels_list[1], 2)] + repeat_block(channels_list[1], channels_list[1], num_repeats[1])
-    self.ERBlock_3 = [RepVGGBlock(channels_list[1], channels_list[2], 2)] + repeat_block(channels_list[2], channels_list[2], num_repeats[2])
-    self.ERBlock_4 = [RepVGGBlock(channels_list[2], channels_list[3], 2)] + repeat_block(channels_list[3], channels_list[3], num_repeats[3])
-    self.ERBlock_5 = [RepVGGBlock(channels_list[3], channels_list[4], 2)] + repeat_block(channels_list[4], channels_list[4], num_repeats[4]) + \
-        + [CSPSPPFModule(channels_list[4], channels_list[4])]
+  def __init__(self, ch_list, num_repeats):
+    self.stem = [RepVGGBlock(3, ch_list[0], stride=2)]
+    self.ERBlock_2 = [RepVGGBlock(ch_list[0], ch_list[1], 2)] + repeat_block(ch_list[1], ch_list[1], num_repeats[1])
+    self.ERBlock_3 = [RepVGGBlock(ch_list[1], ch_list[2], 2)] + repeat_block(ch_list[2], ch_list[2], num_repeats[2])
+    self.ERBlock_4 = [RepVGGBlock(ch_list[2], ch_list[3], 2)] + repeat_block(ch_list[3], ch_list[3], num_repeats[3])
+    self.ERBlock_5 = [RepVGGBlock(ch_list[3], ch_list[4], 2)] + repeat_block(ch_list[4], ch_list[4], num_repeats[4]) + \
+        + [CSPSPPFModule(ch_list[4], ch_list[4])]
 
   def __call__(self, x: Tensor) -> Tuple[Tensor]:
     x1 = x.sequential(self.stem)
@@ -91,17 +91,17 @@ class EfficientRep:
     return (x2, x3, x4, x5)
 
 class RepBiFPANNeck:
-  def __init__(self, channels_list, num_repeats):
-    self.reduce_layer0 = ConvBNReLU(channels_list[4], channels_list[5], 1, 1)
-    self.Bifusion0 = BiFusion([channels_list[3], channels_list[2]], channels_list[5])
-    self.Rep_p4 = repeat_block(channels_list[5], channels_list[5], num_repeats[5])
-    self.reduce_layer1 = ConvBNReLU(channels_list[5], channels_list[6], 1, 1)
-    self.Bifusion1 = BiFusion([channels_list[2], channels_list[1]], channels_list[6])
-    self.Rep_p3 = repeat_block(channels_list[6], channels_list[6], num_repeats[6])
-    self.downsample2 = ConvBNReLU(channels_list[6], channels_list[7], 3, 2)
-    self.Rep_n3 = repeat_block(channels_list[6] + channels_list[7], channels_list[8], num_repeats[7])
-    self.downsample1 = ConvBNReLU(channels_list[8], channels_list[9], 3, 2)
-    self.Rep_n4 = repeat_block(channels_list[5] + channels_list[9], channels_list[10], num_repeats[8])
+  def __init__(self, ch_list, num_repeats):
+    self.reduce_layer0 = ConvBNReLU(ch_list[4], ch_list[5], 1, 1)
+    self.Bifusion0 = BiFusion([ch_list[3], ch_list[2]], ch_list[5])
+    self.Rep_p4 = repeat_block(ch_list[5], ch_list[5], num_repeats[5])
+    self.reduce_layer1 = ConvBNReLU(ch_list[5], ch_list[6], 1, 1)
+    self.Bifusion1 = BiFusion([ch_list[2], ch_list[1]], ch_list[6])
+    self.Rep_p3 = repeat_block(ch_list[6], ch_list[6], num_repeats[6])
+    self.downsample2 = ConvBNReLU(ch_list[6], ch_list[7], 3, 2)
+    self.Rep_n3 = repeat_block(ch_list[6] + ch_list[7], ch_list[8], num_repeats[7])
+    self.downsample1 = ConvBNReLU(ch_list[8], ch_list[9], 3, 2)
+    self.Rep_n4 = repeat_block(ch_list[5] + ch_list[9], ch_list[10], num_repeats[8])
 
   def __call__(self, x: Tuple[Tensor]) -> Tuple[Tensor]:
 
@@ -129,7 +129,7 @@ class RepBiFPANNeck:
 class EffiDeHead:
     '''Efficient Decoupled Head
     '''
-    def __init__(self, num_classes, channels_list):  # detection layer
+    def __init__(self, num_classes, ch_list):  # detection layer
         self.num_classes = num_classes
         self.grid = [Tensor.zeros(1)] * 3
         self.prior_prob = 1e-2
@@ -143,12 +143,12 @@ class EffiDeHead:
         self.stems = self.cls_convs = self.reg_convs = self.cls_preds = self.reg_preds = []
         chx = [6, 8, 10]
         for i in range(3):
-            chnls = channels_list[chx[i]]
-            self.stems.append(ConvBNSiLU(chnls, chnls, 1, 1))
-            self.cls_convs.append(ConvBNSiLU(chnls, chnls, 3, 1))
-            self.reg_convs.append(ConvBNSiLU(chnls, chnls, 3, 1))
-            self.cls_preds.append(nn.Conv2d(chnls, num_classes, 1))
-            self.reg_preds.append(nn.Conv2d(chnls, 4, 1))
+            ch = ch_list[chx[i]]
+            self.stems.append(ConvBNSiLU(ch, ch, 1, 1))
+            self.cls_convs.append(ConvBNSiLU(ch, ch, 3, 1))
+            self.reg_convs.append(ConvBNSiLU(ch, ch, 3, 1))
+            self.cls_preds.append(nn.Conv2d(ch, num_classes, 1))
+            self.reg_preds.append(nn.Conv2d(ch, 4, 1))
 
 
     def __call__(self, x):
@@ -210,12 +210,12 @@ class YOLOv6s:
     num_repeat_backbone = [1, 6, 12, 18, 6]
     num_repeat_neck = [12, 12, 12, 12]
     num_repeat = [(max(round(i * d), 1) if i > 1 else i) for i in (num_repeat_backbone + num_repeat_neck)]
-    channels_list_backbone = [64, 128, 256, 512, 1024]
-    channels_list_neck = [256, 128, 128, 256, 256, 512]
-    channels_list = [math.ceil(i * w / 8) * 8 for i in (channels_list_backbone + channels_list_neck)]
-    self.backbone = EfficientRep(in_channels=3, channels_list=channels_list, num_repeats=num_repeat)
-    self.neck = RepBiFPANNeck(channels_list=channels_list, num_repeats=num_repeat)
-    self.head = EffiDeHead(num_classes, channels_list)
+    ch_list_backbone = [64, 128, 256, 512, 1024]
+    ch_list_neck = [256, 128, 128, 256, 256, 512]
+    ch_list = [math.ceil(i * w / 8) * 8 for i in (ch_list_backbone + ch_list_neck)]
+    self.backbone = EfficientRep(in_ch=3, ch_list=ch_list, num_repeats=num_repeat)
+    self.neck = RepBiFPANNeck(ch_list=ch_list, num_repeats=num_repeat)
+    self.head = EffiDeHead(num_classes, ch_list)
 
   def __call__(self, x: Tensor) -> Tensor:
     x = self.backbone(x)
